9eba489b95c5e87d94c012f66c4d9f14a1ac520f
phandaiduonghcb <phandaiduonghcb@gmail.com>
2022-06-08 17:26:21 +0700

    create project structure

---
+++ src/views/HomeScreen/Home.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import { StyleSheet } from 'react-native';
+
+class MyText extends Component {
+    render () {
+        return (
+            <Text>Hello World!</Text>
+        )
+    }
+}
