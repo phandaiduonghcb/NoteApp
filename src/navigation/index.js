d5a75deba19fb55fd2bef30e00311dd14f456b5b
phandaiduonghcb <phandaiduonghcb@gmail.com>
2022-06-12 15:47:07 +0700

    add group drawer

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import {Layout} from '@ui-kitten/components'
+import { Text, Divider, Layout } from '@ui-kitten/components'
 import HomeScreen from '../views/HomeScreen'
 import TagScreen from '../views/TagScreen'
 import NoteScreen from '../views/NoteScreen';
-import SearchBar from '../components/searchBar'
 
-import {Drawer, DrawerItem, IndexPath} from '@ui-kitten/components'
+import { Drawer, DrawerItem, IndexPath, Icon, DrawerGroup } from '@ui-kitten/components'
 
 import {
   createDrawerNavigator,
@@ -14,15 +13,90 @@ import {
   // DrawerItemList,
   // DrawerItem,
 } from '@react-navigation/drawer';
-const DrawerContent= ({ navigation, state}) => (
-  <Drawer
-    selectedIndex={new IndexPath(state.index)}
-    onSelect={index => navigation.navigate(state.routeNames[index.row])}>
-    <DrawerItem title='Home' />
-    <DrawerItem title='Tag' />
-    <DrawerItem title='Note'/>
-  </Drawer>
-)
+
+
+
+const NoteIcon = (props) => (
+  <Icon {...props} name='credit-card' />
+);
+
+
+const TagIcon = (props) => (
+  <Icon {...props} name='plus-square' />
+);
+
+const LabelIcon = (props) => (
+  <Icon {...props} name='bookmark' />
+);
+
+const BellIcon = (props) => (
+  <Icon {...props} name='bell' />
+);
+
+
+
+const Header = (props) => (
+  <Layout style={{ paddingVertical: '10%' }}>
+    <Text category='h1'>This is header</Text>
+    <Divider />
+  </Layout>
+);
+
+const Footer = (props) => (
+  <Layout style={{ paddingVertical: '10%' }}>
+    <Text category='h1'>This is Footer</Text>
+    <Divider />
+  </Layout>
+);
+
+
+const DrawerContent = ({ navigation, state }) => {
+
+  const navigate = (navigation, index) => {
+    if (index.row==0){
+      navigation.navigate('Home')
+    }
+    else if (index.row==1){
+      // navigation.navigate('')
+    }
+    else if (index.section==undefined && index.row==2)
+    {
+      //Ko lam gi het
+    }
+    else if (index.section==2 && index.row==0)
+    {
+      navigation.navigate('Home')
+    }
+    else if (index.section==2 && index.row==1)
+    {
+      navigation.navigate('Tag')
+    }
+    else if (index.row==3){
+      navigation.navigate('Tag')
+    }
+    else if (index.row==4){
+      navigation.navigate('Note')
+    }
+  }
+
+  return (
+    <Drawer
+      header={Header}
+      footer={Footer}
+      selectedIndex={new IndexPath(state.index)}
+      onSelect={index => {navigate(navigation, index)}}>
+      <DrawerItem accessoryLeft={NoteIcon} title='Home'/>
+      <DrawerItem accessoryLeft={BellIcon} title='Alarm'/>
+      <DrawerGroup accessoryLeft={TagIcon} title='Tag Group'>
+        <DrawerItem accessoryLeft={TagIcon} title='Add Tag' />
+        <DrawerItem accessoryLeft={LabelIcon} title='Tag' />
+      </DrawerGroup>
+
+      <DrawerItem title='Tag' />
+      <DrawerItem title='Note' />
+    </Drawer>
+  )
+}
 
 const { Navigator, Screen } = createDrawerNavigator();
 
@@ -32,9 +106,9 @@ function MyDrawer() {
       useLegacyImplementation
       drawerContent={(props) => <DrawerContent {...props} />}
     >
-      <Screen name="Home" component={HomeScreen} options={{headerShown:false}} />
-      <Screen name="Tag" component={TagScreen} options={{headerShown:false}} />
-      <Screen name="Note" component={NoteScreen} options={{headerShown:false}} />
+      <Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Screen name="Tag" component={TagScreen} options={{ headerShown: false }} />
+      <Screen name="Note" component={NoteScreen} options={{ headerShown: false }} />
     </Navigator>
   );
 }
