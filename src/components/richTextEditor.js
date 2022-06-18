66546c1d5d0325334d5b1d9eaa4c6bac23d349d7
phandaiduonghcb <phandaiduonghcb@gmail.com>
2022-06-18 15:56:01 +0700

    add Richtext editor

---
+++ src/components/richTextEditor.js
@@ -0,0 +1,167 @@
+import React from "react";
+import {Platform, KeyboardAvoidingView, StyleSheet, ScrollView, Keyboard, Dimensions } from "react-native";
+import { actions, RichEditor, RichToolbar } from "react-native-pell-rich-editor";
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry, Layout, useTheme } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { Text, Button, Card, Modal, Divider, Icon } from '@ui-kitten/components'
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+let Y = 0
+let HEIGHT = 0
+let HTML = ''
+const CameraIcon = (props) => (
+    <Icon name='camera' {...props} />
+);
+
+const UploadIcon = (props) => (
+    <Icon name='upload' {...props} />
+);
+
+const RichTextEditor = (props) => {
+
+    let handleCursorPosition = React.useCallback((scrollY) => {
+        // Positioning scroll bar
+        scrollRef.current.scrollTo({ y: scrollY - 30, animated: true });
+    }, [])
+
+    const openCamera = (isCameraLaunched) => {
+        let options = {
+            mediaType: 'photo',
+            includeBase64: true,
+        }
+        let result
+        if (isCameraLaunched) {
+            launchCamera(options, (response => {
+                if (response.assets != undefined) {
+                    result = response.assets[0].base64
+                    // console.log(result)
+                    html = '<img src="data:image/png;base64,' + result + '" />'
+                    richText.current?.insertHTML(html)
+                    // a = count+1
+                    // setCount(a)
+                }
+            }));
+        }
+        else{
+            launchImageLibrary(options, (response => {
+                if (response.assets != undefined) {
+                    result = response.assets[0].base64
+                    // console.log(result)
+                    html = '<img src="data:image/png;base64,' + result + '" />'
+                    richText.current?.insertHTML(html)
+                    // a = count+1
+                    // setCount(a)
+                }
+            }));
+        }
+    }
+    React.useEffect(() => {
+        
+        const showSubscription = Keyboard.addListener("keyboardDidShow", (e) => {
+
+            let keyBoardHeight = e.endCoordinates.height
+            maxY = windowHeight - keyBoardHeight
+            let newHeight
+            if (maxY < Y) {
+                newHeight = HEIGHT - Math.round(Y - maxY) - 10
+                setEHeight(newHeight-130);
+            }
+            setKeyboardStatus("Keyboard Shown");
+        });
+        const hideSubscription = Keyboard.addListener("keyboardDidHide", (e) => {
+            setKeyboardStatus("Keyboard Hidden");
+            setEHeight(props.height)
+        });
+
+        return () => {
+            showSubscription.remove();
+            hideSubscription.remove();
+        };
+    }, []);
+
+    const theme = useTheme();
+
+    const [keyboardStatus, setKeyboardStatus] = React.useState(undefined);
+    const [eHeight, setEHeight] = React.useState(props.height);
+    const richText = React.useRef();
+    const scrollRef = React.useRef()
+    const [visible, setVisible] = React.useState(false);
+
+    return (
+        <Layout style={{ height: eHeight , marginHorizontal:'3%'}}
+            onLayout={event => {
+                const layout = event.nativeEvent.layout;
+                Y = (layout.y + layout.height)
+                HEIGHT = layout.height
+            }}
+        >
+            <ScrollView ref={scrollRef}>
+                <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 1 }}>
+                    <RichEditor
+
+                        ref={richText}
+                        onChange={descriptionText => {
+                            // console.log("descriptionText:", descriptionText);
+                            if (descriptionText != ''){
+                                HTML = descriptionText;
+                            }
+                        }}
+                        placeholder={'Type your note here!'}
+                        onCursorPosition={handleCursorPosition}
+                        onFocus={handleCursorPosition}
+                        editorStyle={{ backgroundColor: theme['background-basic-color-2'], color: theme["text-basic-color"] }}
+                    />
+                </KeyboardAvoidingView>
+            </ScrollView>
+
+            <RichToolbar
+                editor={richText}
+                iconTint={theme['color-primary-500']}
+                selectedIconTint={theme['color-danger-500']}
+                actions={[actions.setBold, actions.setItalic, actions.setUnderline, actions.insertImage, actions.checkboxList, 'exitKeyboard']}
+                onPressAddImage={() => {
+                    // openCamera()
+                    setVisible(true)
+                    // scrollRef.current.scrollTo({ y: 0, animated: true });
+                    // richText.current?.commandDOM(`document.execCommand('insertHTML', false, "<br/>diaskdja")`);
+                }}
+                iconMap={{ exitKeyboard: (tintColor) => (<Text style={[{ color: theme['color-primary-500'] }]}>X</Text>), }}
+                exitKeyboard={() => richText.current?.dismissKeyboard()}
+            />
+            {/* <Text>{keyboardStatus}</Text> */}
+            {/* <View style={styles.container}> */}
+            <Modal
+                visible={visible}
+                backdropStyle={styles.backdrop}
+                onBackdropPress={() => setVisible(false)}>
+                <Card disabled={true}>
+                    <Button accessoryLeft={CameraIcon} onPress={() => {
+                        setVisible(false)
+                        openCamera(true)
+                    }}>
+                        Take a photo
+                    </Button>
+                    <Divider style={{ marginVertical: '2%' }} />
+                    <Button accessoryLeft={UploadIcon} onPress={()=>{
+                        setVisible(false)
+                        openCamera(false)
+                    }}>
+                        Choose from libary
+                    </Button>
+                </Card>
+            </Modal>
+        </Layout>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    backdrop: {
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+});
+export default RichTextEditor
--- src/views/NoteScreen/index.js
+++ src/views/NoteScreen/index.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import {Alert,Pressable,StyleSheet ,Dimensions,SafeAreaView} from 'react-native';
-import {Input, Button, Divider, Icon, Text, TopNavigation, TopNavigationAction,Tab, TabBar ,BottomNavigation, BottomNavigationTab,Layout, Menu,  MenuItem,OverflowMenu  } from '@ui-kitten/components';
+import { Alert, Pressable, StyleSheet, Dimensions, SafeAreaView, View } from 'react-native';
+import { Input, Button, Divider, Icon, Text, TopNavigation, TopNavigationAction, Tab, TabBar, BottomNavigation, BottomNavigationTab, Layout, Menu, MenuItem, OverflowMenu } from '@ui-kitten/components';
 import { ScrollView } from 'react-native-gesture-handler';
 import BottomSheetAdd from '../../components/bottomSheetAdd';
 
 import { Provider } from 'react-native-paper';
 import BottomSettingNote from '../../components/bottomSettingNote';
 import SelectDate from '../../components/selectDate';
+import RichTextEditor from '../../components/richTextEditor';
 
 // var AntDesignICon = require('react-native-vector-icons/AntDesign')
 const PinIcon = (props) => (
-  <Icon {...props}  name='pin' pack='material' />
-  
+  <Icon {...props} name='pin' pack='material' />
+
 );
-const UnPinIcon = (props) => 
+const UnPinIcon = (props) =>
 (
   <Icon {...props} name='pin-off' pack='material' />
 )
-const ArchiveIcon = (props) => 
+const ArchiveIcon = (props) =>
 (
-  <Icon {...props} name='archive-arrow-down'  pack='material' />
+  <Icon {...props} name='archive-arrow-down' pack='material' />
 );
-const UnArchiveIcon = (props) => 
+const UnArchiveIcon = (props) =>
 (
-  <Icon {...props} name='archive-arrow-up'  pack='material' />
+  <Icon {...props} name='archive-arrow-up' pack='material' />
 );
 
 const BackIcon = (props) => (
@@ -31,19 +32,19 @@ const BackIcon = (props) => (
 );
 
 const BellIcon = (props) => (
-  <Icon {...props} name='bell-outline'/>
+  <Icon {...props} name='bell-outline' />
 );
 const DotsVeticalIcon = (props) => (
- <Icon {...props} name='dots-vertical' pack='material'/>
+  <Icon {...props} name='dots-vertical' pack='material' />
 );
 const PersonIcon = (props) => (
-  <Icon {...props} name='person-outline'/>
+  <Icon {...props} name='person-outline' />
 );
 const AddIcon = (props) => (
-  <Icon {...props} name='plus-square-outline'/>
+  <Icon {...props} name='plus-square-outline' />
 );
 const EmailIcon = (props) => (
-  <Icon {...props} name='email-outline'/>
+  <Icon {...props} name='email-outline' />
 );
 const useInputState = (initialValue = '') => {
   const [value, setValue] = React.useState(initialValue);
@@ -61,27 +62,27 @@ const useBottomNavigationState = (initialState = 0) => {
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-
+let titleY
 const NoteScreen = ({ navigation, route }) => {
   // const topState = useTabBarState();
-  const find_dimesions = (layout)=>{
-    const {x, y, width, height} = layout;
+  const find_dimesions = (layout) => {
+    const { x, y, width, height } = layout;
     console.warn(x);
     console.warn(y);
     console.warn(width);
     console.warn(height);
     return height;
   };
-  const [EditTextState,setEditTextState] = React.useState("Thời gian chỉnh sửa note");
-  const [PinState,setPinSate] = React.useState(false);
-  const [ArchiveState,setArchiveState] = React.useState(false);
+  const [EditTextState, setEditTextState] = React.useState("Thời gian chỉnh sửa note");
+  const [PinState, setPinSate] = React.useState(false);
+  const [ArchiveState, setArchiveState] = React.useState(false);
   const TitleState = useInputState();
   const NoteState = useInputState();
   const topState = useBottomNavigationState();
   const [menuVisible, setMenuVisible] = React.useState(false);
-  const [showBotoomSheetAdd,setshowBotoomSheetAdd] = React.useState(false);
-  const [showBotoomSettingNote,setshowBotoomSettingNote] = React.useState(false);
-  const [showSelectDate,setshowSelectDate] = React.useState(false);
+  const [showBotoomSheetAdd, setshowBotoomSheetAdd] = React.useState(false);
+  const [showBotoomSettingNote, setshowBotoomSettingNote] = React.useState(false);
+  const [showSelectDate, setshowSelectDate] = React.useState(false);
   const toggleMenu = () => {
     setMenuVisible(!menuVisible);
   };
@@ -90,79 +91,76 @@ const NoteScreen = ({ navigation, route }) => {
   //   <TopNavigationAction icon={DotsVeticalIcon} onPress={toggleMenu}/>
   // );
   const BackAction = () => (
-    <TopNavigationAction icon={BackIcon} onPress={() => navigation.goBack()}/>
+    <TopNavigationAction icon={BackIcon} onPress={() => navigation.goBack()} />
 
   );
- 
+
   const renderRightTopNavigationAction = () =>
   (
     <React.Fragment >
-    <TopNavigationAction icon={PinState ? PinIcon : UnPinIcon} onPress={() => setPinSate(!PinState)}/>
-    <TopNavigationAction icon={BellIcon } onPress={() => {
-      console.log("select date");
-      setshowSelectDate(!showSelectDate);
+      <TopNavigationAction icon={PinState ? PinIcon : UnPinIcon} onPress={() => setPinSate(!PinState)} />
+      <TopNavigationAction icon={BellIcon} onPress={() => {
+        console.log("select date");
+        setshowSelectDate(!showSelectDate);
 
-    }}/>
-    <TopNavigationAction icon={ArchiveState ? UnArchiveIcon : ArchiveIcon} onPress={() => setArchiveState(!ArchiveState)}/>
+      }} />
+      <TopNavigationAction icon={ArchiveState ? UnArchiveIcon : ArchiveIcon} onPress={() => setArchiveState(!ArchiveState)} />
     </React.Fragment>
-   
-   
+
+
   );
 
-  
 
-    return (
-      <Provider>
 
-     
-     <Layout style={{flex: 1, flexDirection: 'column',}}>
+  return (
+    <Provider>
 
-     
-      {/* <React.Fragment> */}
 
-        <TopNavigation style={{height:10,flexBasis:10}} accessoryLeft={BackAction} accessoryRight={renderRightTopNavigationAction}/>
-      
-      {/* <Divider/> */}
-      <ScrollView style={{height:windowHeight}}>
-        <Input
-          style={{padding: 10,}}
-          multiline={true}
-          
-          placeholder='Title'
-          {...TitleState}
-        />
-      {/* chỗ nhập nội dung note   */}
-       <Input
-        style={{ padding: 10,height:'50%'}}
-         
-          multiline={true}
-    
-          placeholder='Note'
-          {...NoteState}
-        />
+      <Layout style={{ flex: 1, flexDirection: 'column', }}>
 
+
+        {/* <React.Fragment> */}
+
+        <TopNavigation style={{ height: 10, flexBasis: 10 }} accessoryLeft={BackAction} accessoryRight={renderRightTopNavigationAction} />
+
+        {/* <Divider/> */}
+        <ScrollView style={{ height: windowHeight }}>
+          <Input
+            style={{ padding: 10, }}
+            multiline={true}
+
+            placeholder='Title'
+            {...TitleState}
+
+            onLayout={event => {
+              const layout = event.nativeEvent.layout;
+              titleY = layout.y
+            }}
+          />
+          {/* chỗ nhập nội dung note   */}
+          <RichTextEditor height={windowHeight*75/100} />
         </ScrollView>
-        
+
         {/* <BottomNavigation style={{ height:10,flex: 1, flexDirection: 'row',}} {...topState}> */}
-        <Layout style={{ flexDirection: 'row',    }}>
+        <Layout style={{ flexDirection: 'row', }}>
 
-       
+
           {/* <BottomNavigationTab icon={AddIcon} style={{width:5,alignItems:'center'}}/> */}
-          <Button style={{margin: 2,}} accessoryLeft={AddIcon} appearance='ghost' status='basic' size='large' onPress={()=> setshowBotoomSheetAdd(true)}></Button>
+          <Button style={{ margin: 2, }} accessoryLeft={AddIcon} appearance='ghost' status='basic' size='large' onPress={() => setshowBotoomSheetAdd(true)}></Button>
 
-          <Text style={{flex:1,textAlign: 'center',fontSize: 15,marginTop: 19,}} >{EditTextState} </Text>
-          
-          <Button style={{margin:2,}} accessoryLeft={DotsVeticalIcon} appearance='ghost' status='basic' size='large' onPress={() => setshowBotoomSettingNote(true)}/>
-         
-          </Layout>
+          <Text style={{ flex: 1, textAlign: 'center', fontSize: 15, marginTop: 19, }} >{EditTextState} </Text>
+
+          <Button style={{ margin: 2, }} accessoryLeft={DotsVeticalIcon} appearance='ghost' status='basic' size='large' onPress={() => setshowBotoomSettingNote(true)} />
+
+        </Layout>
         {/* </BottomNavigation> */}
-      {/* </React.Fragment> */}
-      <BottomSheetAdd show={showBotoomSheetAdd} onDismiss={() => {setshowBotoomSheetAdd(false)}}></BottomSheetAdd>
-      <BottomSettingNote show={showBotoomSettingNote}  onDismiss={() => setshowBotoomSettingNote(false)}/>
-      <SelectDate isVisible={showSelectDate} onBackButtonPress={() => setshowSelectDate(false)} onBackdropPress={() => setshowSelectDate(false)}></SelectDate>
+        {/* </React.Fragment> */}
+        <BottomSheetAdd show={showBotoomSheetAdd} onDismiss={() => { setshowBotoomSheetAdd(false) }}></BottomSheetAdd>
+        <BottomSettingNote show={showBotoomSettingNote} onDismiss={() => setshowBotoomSettingNote(false)} />
+        <SelectDate isVisible={showSelectDate} onBackButtonPress={() => setshowSelectDate(false)} onBackdropPress={() => setshowSelectDate(false)}></SelectDate>
       </Layout>
-      </Provider>
-    )
+    </Provider>
+  )
 }
 
 const styles = StyleSheet.create({
