1a2547275ed97665059d7b23e4b9a25a032c2f3c
hauphanlvc <hauphanlvc@gmail.com>
2022-06-26 14:58:02 +0700

    fix refresh home screen

--- src/views/HomeScreen/index.js
+++ src/views/HomeScreen/index.js
@@ -37,7 +37,7 @@ const HomeScreen = ({ navigation }) => {
     <Layout>
       <SearchBar navigation={navigation} />
     </Layout>
-      <NoteSortableList />
+      <NoteSortableList navigation={navigation}/>
       <BottomBar></BottomBar>
       <ThemedActionButton navigation={navigation} />
     </>
